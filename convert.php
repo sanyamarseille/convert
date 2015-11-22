aca6d60bbf551635cbdbc3766b6626a2af39c4c8
root <root@localhost.localdomain>
2015-11-22 21:53:51 +0900

    base file

---
+++ convert.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+    <title> convert system : result</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    converted<br><br>
+  </head>
+  <body>
+    <?php
+       if((strlen($_POST["type"]) > 0) && (strlen($_POST["string"]) > 0))
+    {
+    if($_POST["type"] == "url_encode"){
+    $string = mb_convert_encoding($_POST["string"],$_POST["charcode"]);
+    $result = urlencode($string);
+    }
+    elseif($_POST["type"] == "url_decode"){
+    $after = urldecode($_POST["string"]);
+    $result = htmlspecialchars($after,ENT_HTML401,$_POST["charcode"]);
+    }
+    elseif($_POST["type"] == "base64_encode"){
+    $string = mb_convert_encoding($_POST["string"],$_POST["charcode"]);
+    $result = base64_encode($string);
+    }
+    elseif($_POST["type"] == "base64_decode"){
+    $after = base64_decode($_POST["string"]);
+    $result = htmlspecialchars($after,ENT_HTML401,$_POST["charcode"]);
+    }
+    elseif($_POST["type"] == "rot13"){
+    $string = mb_convert_encoding($_POST["string"],$_POST["charcode"]);
+    $result = str_rot13($string);
+    }
+    print($result);
+    }
+    else
+    {
+    print("check type and string");
+    }
+    ?>
+    <br><br>
+    <form>
+      <input type="button" value="retrun" onClick="history.back()">
+    </form>
+  </body>
+</html>
